1ec08e7bcd78c8df5a576bc8b0430e4e66813d3e
mohadeev <94206815+mohadeev@users.noreply.github.com>
2022-04-24 15:47:39 +0200

    df

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -4,13 +4,16 @@ import GlobalStyles from "src/components/GlobalStyles";
 import { NextPage } from "next";
 import { AppProps } from "next/dist/next-server/lib/router/router";
 
-const App: NextPage<AppProps> = ({ Component, pageProps }) => {
+const App = ({ Component, pageProps }) => {
   return (
     <>
       <GlobalStyles />
       <Component {...pageProps} />
+    
     </>
   );
 };
 
 export default App;
+
+//
